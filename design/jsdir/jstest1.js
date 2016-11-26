b9a19ccfb98b6444910536aec8ca1d3c9fd24920
yahaa <1477765176@qq.com>
2016-11-26 14:57:24 +0800

    update

---
+++ design/jsdir/jstest1.js
@@ -0,0 +1,24 @@
+/**
+ * Created by zihua on 16-11-26.
+ */
+function changeColor(){
+    element=document.getElementById("off_pic");
+    if(element.src.match("bulboff")){
+        element.src="images/pic_bulbon.gif";
+    }
+    else{
+        element.src="images/pic_bulboff.gif";
+    }
+}
+
+function frontcolor() {
+    element=document.getElementById("fronts");
+    element.style.color="#ee0000";
+}
+
+function checkNumber(){
+    var x=document.getElementById("inputs");
+    if(x==""||isNaN(x)){
+        alert("sb");
+    }
+}
---
+++ jsdir/jstest1.js
@@ -0,0 +1,3 @@
+/**
+ * Created by zihua on 16-11-26.
+ */
